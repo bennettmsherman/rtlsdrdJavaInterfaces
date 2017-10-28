e98691a9cb871f5ffdb6464bf7d17a225f703623
Ben Sherman <bennettmsherman@gmail.com>
2017-10-28 19:41:37 -0400

    Fix formatting

--- src/com/bensherman/rtlsdrdjava/tcpcli/Message.java
+++ src/com/bensherman/rtlsdrdjava/tcpcli/Message.java
@@ -1,125 +1,132 @@
 package com.bensherman.rtlsdrdjava.tcpcli;
 
-public class Message {
+public class Message
+{
 
-	public enum ResponseType
-	{
-		OK, ERROR;
-	}
-	
-	/**
-	 * The message sent to the server
-	 */
-	private final String outboundMsg;
-	
-	/**
-	 * The server's response message.
-	 * Can only be set once externally.
-	 */
-	private String responseMsg;
-	
-	/**
-	 * Whether or not the responseMsg field has been set.
-	 */
-	private boolean hasResponseBeenSet;
-	
-	/**
-	 * If this message's response represented an OK result or an error.
-	 * Can only be set once externally.
-	 */
-	private ResponseType responseMsgType;
-	
-	/**
-	 * Whether of not the responseType field has been set.
-	 */
-	private boolean hasResponseTypeBeenSet;
-	
-	/**
-	 * Constructs a new Message given a message to be sent
-	 * @param outboundMsg The message to send
-	 */
-	public Message(String outboundMsg)
-	{
-		this.outboundMsg = outboundMsg;
-		hasResponseBeenSet = false;
-		responseMsgType = ResponseType.OK;
-		hasResponseTypeBeenSet = false;
-	}
-	
-	/**
-	 * @return The outbound/sent message
-	 */
-	public String getOutboundMsg()
-	{
-		return outboundMsg;
-	}
-	
-	/**
-	 * @return The response message
-	 */
-	public String getResponseMsg()
-	{
-		return responseMsg;
-	}
-	
-	/**
-	 * @return	The response message's type
-	 */
-	public ResponseType getResponseMsgType()
-	{
-		return responseMsgType;
-	}
-	
-	/**
-	 * Sets the response of this message
-	 * @param responseMsg The response to be set
-	 * @throws IllegalArgumentException	If this method is called once
-	 * 	responseMsg has already been set
-	 */
-	public synchronized void setResponseMessage(String responseMsg) throws IllegalArgumentException
-	{
-		if (!hasResponseBeenSet)
-		{
-			this.responseMsg = responseMsg;	
-			hasResponseBeenSet = true;
-		}
-		else
-		{
-			throw new IllegalArgumentException("responseMsg can only be set once!");
-		}
-	}
-	
-	/**
-	 * Sets the ResponseType of this message.
-	 * @param responseMsgType	The new value of responseType
-	 * @throws IllegalArgument 	If this method is called once responseType has
-	 * 	already been set.
-	 */
-	public synchronized void setResponseType(ResponseType responseMsgType)
-	{
-		if (!hasResponseTypeBeenSet)
-		{
-			this.responseMsgType = responseMsgType;
-			hasResponseTypeBeenSet = true;
-		}
-		else
-		{
-			throw new IllegalArgumentException("msgType can only be set once");
-		}
-	}
+    public enum ResponseType
+    {
+        OK, ERROR;
+    }
 
-	/**
-	 * Basic toString() implementation to describe a Message
-	 */
-	public String toString()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("OUTBOUND/SEND MESSAGE: ");
-		builder.append(outboundMsg);
-		builder.append("\n\tRESPONSE MESSAGE:");
-		builder.append(responseMsg);
-		builder.append("\n\tRESPONSE MESSAGE TYPE: ");
-		builder.append(responseMsgType.toString());
-		return builder.toString();
-	}
+    /**
+     * The message sent to the server
+     */
+    private final String outboundMsg;
+
+    /**
+     * The server's response message. Can only be set once externally.
+     */
+    private String responseMsg;
+
+    /**
+     * Whether or not the responseMsg field has been set.
+     */
+    private boolean hasResponseBeenSet;
+
+    /**
+     * If this message's response represented an OK result or an error. Can only be
+     * set once externally.
+     */
+    private ResponseType responseMsgType;
+
+    /**
+     * Whether of not the responseType field has been set.
+     */
+    private boolean hasResponseTypeBeenSet;
+
+    /**
+     * Constructs a new Message given a message to be sent
+     *
+     * @param outboundMsg
+     *            The message to send
+     */
+    public Message(final String outboundMsg)
+    {
+        this.outboundMsg = outboundMsg;
+        hasResponseBeenSet = false;
+        responseMsgType = ResponseType.OK;
+        hasResponseTypeBeenSet = false;
+    }
+
+    /**
+     * @return The outbound/sent message
+     */
+    public String getOutboundMsg()
+    {
+        return outboundMsg;
+    }
+
+    /**
+     * @return The response message
+     */
+    public String getResponseMsg()
+    {
+        return responseMsg;
+    }
+
+    /**
+     * @return The response message's type
+     */
+    public ResponseType getResponseMsgType()
+    {
+        return responseMsgType;
+    }
+
+    /**
+     * Sets the response of this message
+     *
+     * @param responseMsg
+     *            The response to be set
+     * @throws IllegalArgumentException
+     *             If this method is called once responseMsg has already been set
+     */
+    public synchronized void setResponseMessage(final String responseMsg) throws IllegalArgumentException
+    {
+        if (!hasResponseBeenSet)
+        {
+            this.responseMsg = responseMsg;
+            hasResponseBeenSet = true;
+        }
+        else
+        {
+            throw new IllegalArgumentException("responseMsg can only be set once!");
+        }
+    }
+
+    /**
+     * Sets the ResponseType of this message.
+     *
+     * @param responseMsgType
+     *            The new value of responseType
+     * @throws IllegalArgument
+     *             If this method is called once responseType has already been set.
+     */
+    public synchronized void setResponseType(final ResponseType responseMsgType)
+    {
+        if (!hasResponseTypeBeenSet)
+        {
+            this.responseMsgType = responseMsgType;
+            hasResponseTypeBeenSet = true;
+        }
+        else
+        {
+            throw new IllegalArgumentException("msgType can only be set once");
+        }
+    }
+
+    /**
+     * Basic toString() implementation to describe a Message
+     */
+    @Override
+    public String toString()
+    {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("OUTBOUND/SEND MESSAGE: ");
+        builder.append(outboundMsg);
+        builder.append("\n\tRESPONSE MESSAGE:");
+        builder.append(responseMsg);
+        builder.append("\n\tRESPONSE MESSAGE TYPE: ");
+        builder.append(responseMsgType.toString());
+        return builder.toString();
+    }
 }
--- src/com/bensherman/rtlsdrdjava/tcpcli/TcpSocketWriter.java
+++ src/com/bensherman/rtlsdrdjava/tcpcli/TcpSocketWriter.java
@@ -12,123 +12,131 @@ import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-class TcpSocketWriter implements Runnable {
+class TcpSocketWriter implements Runnable
+{
 
-	/**
-	 * New messages to be sent to the server are placed in this queue. This
-	 * class's thread will pend on this queue when empty, until a new message
-	 * is placed into it. The messages in this queue should only have
-	 * their outboundMsg field set.
-	 */
-	private final BlockingQueue<Message> sendMsgQueue;
-	
-	/**
-	 * When set to false, if this thread is interrupted, it will die.
-	 */
-	private boolean keepAlive;
-	
-	/**
-	 * PrintWriter instance which is tied to the socket's output stream.
-	 */
-	private final PrintWriter tcpSocketWriter;
-	
-	/**
-	 * The TcpClient which instantiated this class.
-	 */
-	private final TcpClient tcpClient;
-	
-	/**
-	 * Constructs a new TcpSocketWriter.
-	 * @param socketToWriteTo	The socket which is connected to the server,
-	 * 	which this thread will write to.
-	 * @param tcpClient	The TcpClient which instantiated this class
-	 * @throws IOException 	In the event that there was an error retrieving
-	 * 	the output stream of the socket param
-	 */
-	TcpSocketWriter(Socket socketToWriteTo, TcpClient tcpClient) throws IOException
-	{
-		tcpSocketWriter = new PrintWriter(socketToWriteTo.getOutputStream(), true);
-		keepAlive = true;
-		sendMsgQueue = new LinkedBlockingQueue<>();
-		this.tcpClient = tcpClient;
-	}
-	
-	/**
-	 * The function called when this class is executed as a thread.
-	 * this thread will pend on the sendMsgQueue, waiting for new messages
-	 * to be submitted. When a new message has been submitted, this thread
-	 * will retrieve it, remove it from the queue, and send it to the server.
-	 * Then, the message will be added to the msgsWaitingForResponseQueue
-	 * owned by the TcpSocketReader class/thread.
-	 */
-	@Override
-	public void run() {
-		logMsg("entering run()");
-		
-		try {
-			while (keepAlive)
-			{
-				try 
-				{
-					// Wait for a new message to be submitted, and a new msg
-					// has been submitted, send it.
-					write(sendMsgQueue.take());
-				}
-				catch (InterruptedException exception)
-				{
-					logMsg("Exception caught: " + exception.toString());
-				}
-			}
-		}
-		finally
-		{
-			tcpSocketWriter.close();
-		}
-		
-		logMsg("exiting run()");
-	}
-	
-	/**
-	 * Writes the message specified by the parameter, followed by a newline
-	 * to the server. The message is also added to the
-	 * msgsWaitingForResponseQueue owned by the TcpSocketReader instance
-	 * obtained from the TcpClient
-	 */
-	private void write(Message msg)
-	{
-		logMsg("Writing msg: \"" + msg.getOutboundMsg() + "\"");
-		tcpClient.getTcpSocketReader().addToWaitingForResponseQueue(msg);
-		tcpSocketWriter.println(msg.getOutboundMsg());
-		tcpSocketWriter.flush();
-	}
-	
-	/**
-	 * Creates a Message based off of the parameter, and adds the new Message
-	 * to sendMsgQueue
-	 * @param	The message to be sent
-	 */
-	void enqueueMessage(String msg)
-	{
-		sendMsgQueue.add(new Message(msg));
-	}
-	
-	/**
-	 * Logs/outputs a message. Replace the System.out.println() with a
-	 * logger if desired. The message format is "<ThreadName>: msg"
-	 * @param msg The msg to log
-	 */
-	private void logMsg(String msg)
-	{
-		System.out.println(Thread.currentThread().getName() + ": " + msg);
-	}
-	
-	/**
-	 * Sets the keepAlive flag to false. If the thread executing run() is
-	 * interrupted, thread will die.
-	 */
-	synchronized void requestStop()
-	{
-		keepAlive = false;
-		logMsg("TcpSocketWriter keepAlive set to false");
-	}
+    /**
+     * New messages to be sent to the server are placed in this queue. This class's
+     * thread will pend on this queue when empty, until a new message is placed into
+     * it. The messages in this queue should only have their outboundMsg field set.
+     */
+    private final BlockingQueue<Message> sendMsgQueue;
+
+    /**
+     * When set to false, if this thread is interrupted, it will die.
+     */
+    private boolean keepAlive;
+
+    /**
+     * PrintWriter instance which is tied to the socket's output stream.
+     */
+    private final PrintWriter tcpSocketWriter;
+
+    /**
+     * The TcpClient which instantiated this class.
+     */
+    private final TcpClient tcpClient;
+
+    /**
+     * Constructs a new TcpSocketWriter.
+     * 
+     * @param socketToWriteTo
+     *            The socket which is connected to the server, which this thread
+     *            will write to.
+     * @param tcpClient
+     *            The TcpClient which instantiated this class
+     * @throws IOException
+     *             In the event that there was an error retrieving the output stream
+     *             of the socket param
+     */
+    TcpSocketWriter(final Socket socketToWriteTo, final TcpClient tcpClient) throws IOException
+    {
+        tcpSocketWriter = new PrintWriter(socketToWriteTo.getOutputStream(), true);
+        keepAlive = true;
+        sendMsgQueue = new LinkedBlockingQueue<>();
+        this.tcpClient = tcpClient;
+    }
+
+    /**
+     * The function called when this class is executed as a thread. this thread will
+     * pend on the sendMsgQueue, waiting for new messages to be submitted. When a
+     * new message has been submitted, this thread will retrieve it, remove it from
+     * the queue, and send it to the server. Then, the message will be added to the
+     * msgsWaitingForResponseQueue owned by the TcpSocketReader class/thread.
+     */
+    @Override
+    public void run()
+    {
+        logMsg("entering run()");
+
+        try
+        {
+            while (keepAlive)
+            {
+                try
+                {
+                    // Wait for a new message to be submitted, and a new msg
+                    // has been submitted, send it.
+                    write(sendMsgQueue.take());
+                }
+                catch (final InterruptedException exception)
+                {
+                    logMsg("Exception caught: " + exception.toString());
+                }
+            }
+        }
+        finally
+        {
+            tcpSocketWriter.close();
+        }
+
+        logMsg("exiting run()");
+    }
+
+    /**
+     * Writes the message specified by the parameter, followed by a newline to the
+     * server. The message is also added to the msgsWaitingForResponseQueue owned by
+     * the TcpSocketReader instance obtained from the TcpClient
+     */
+    private void write(final Message msg)
+    {
+        logMsg("Writing msg: \"" + msg.getOutboundMsg() + "\"");
+        tcpClient.getTcpSocketReader().addToWaitingForResponseQueue(msg);
+        tcpSocketWriter.println(msg.getOutboundMsg());
+        tcpSocketWriter.flush();
+    }
+
+    /**
+     * Creates a Message based off of the parameter, and adds the new Message to
+     * sendMsgQueue
+     * 
+     * @param The
+     *            message to be sent
+     */
+    void enqueueMessage(final String msg)
+    {
+        sendMsgQueue.add(new Message(msg));
+    }
+
+    /**
+     * Logs/outputs a message. Replace the System.out.println() with a logger if
+     * desired. The message format is "<ThreadName>: msg"
+     * 
+     * @param msg
+     *            The msg to log
+     */
+    private void logMsg(final String msg)
+    {
+        System.out.println(Thread.currentThread().getName() + ": " + msg);
+    }
+
+    /**
+     * Sets the keepAlive flag to false. If the thread executing run() is
+     * interrupted, thread will die.
+     */
+    synchronized void requestStop()
+    {
+        keepAlive = false;
+        logMsg("TcpSocketWriter keepAlive set to false");
+    }
 }
